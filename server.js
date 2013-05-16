4588398c175e33d439e02306a3bce7bd07bd8ee4
Richard Batty <rjlbatty@gmail.com>
2013-05-16 15:12:10 +0100

    Delete partial application helper function as it is no longer needed.

--- server.js
+++ server.js
@@ -36,21 +36,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-Function.prototype.partial = function(){
-    var fn = this 
-      , args = Array.prototype.slice.call(arguments);
-    return function(){
-      var arg = 0;
-      // Note that here 'arguments' refers to the arguments for the inner function
-      // It is a different set of arguments from one above that is turned into an array
-      // and assigned to 'args'
-      for ( var i = 0; i < args.length && arg < arguments.length; i++ )
-        if ( args[i] === undefined )
-          args[i] = arguments[arg++];
-      return fn.apply(this, args);
-    };
-  };
-
 var DataGetter = function(auth) {
   // Constructor function
   // Taks an auth string (a base64-endocded authorisation string)
