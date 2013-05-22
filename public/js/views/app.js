0b35e85317916a4c123f19b47b9ad7ada05a0aab
Richard Batty <rjlbatty@gmail.com>
2013-05-22 13:40:39 +0100

    Make view list users, workspaces, and projects.

--- public/js/views/app.js
+++ public/js/views/app.js
@@ -9,9 +9,21 @@ app.AppView = Backbone.View.extend({
 
   render: function() {
     var element = this.$el;
+    element.append('<h2>Users</h2>');
+    app.users.each(function(project) {
+      var template = _.template($('#project-template').html(),  {name: project.get("name") });
+      element.append( template );
+    });
+    element.append('<h2>Workspaces</h2>');
+    app.workspaces.each(function(project) {
+      var template = _.template($('#project-template').html(),  {name: project.get("name") });
+      element.append( template );
+    });
+    element.append('<h2>Projects</h2>');
     app.projects.each(function(project) {
       var template = _.template($('#project-template').html(),  {name: project.get("name") });
       element.append( template );
     });
+
   }
 });
