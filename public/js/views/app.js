ad9a592ca75ea295883872b2425af228504fd326
Richard Batty <rjlbatty@gmail.com>
2013-05-24 14:16:47 +0100

    Make it so the order of resetting collections on page load doesn't matter.
    Made a function that returns a function such that every time it is executed,
    it counts up one. If the count reaches a set value, then the next time it is executed
    it renders all the views.

--- public/js/views/app.js
+++ public/js/views/app.js
@@ -1,15 +1,40 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
-  
+
   initialize: function() {
-    this.listenTo(app.projects, 'reset', this.renderProjects);
-    this.listenTo(app.users, 'reset', this.renderUsers);
-    this.listenTo(app.workspaces, 'reset', this.renderWorkspaces);
+    initialRenderer = this.renderer(3);
+    this.listenTo(app.projects, 'reset', initialRenderer);
+    this.listenTo(app.users, 'reset', initialRenderer);
+    this.listenTo(app.workspaces, 'reset', initialRenderer);
+  },
+
+  renderer: function(max_calls) {
+    // Returns a function that you call every time a collection
+    // is reset. If the function has been called max_calls times already
+    // then it executes renderAll, which renders all the views.
+    // If the function has been called less than max_calls then it
+    // just increments the counter.
+    var count = 0;
+    return function() {
+      count++;
+      console.log(count);
+      if (!(count < max_calls)) {
+          this.renderAll();  
+      }
+    }
+  },
+
+  renderAll: function() {
+    this.renderUsers();
+    this.renderWorkspaces();
+    this.renderProjects();
   },
 
   renderUsers: function() {
     var element = $('#users-list');
+    element.empty();
+    element.append('<h2>Users</h2>');
     app.users.each(function(user) {
       var template = _.template($('#user-template').html(),  {name: user.get("name") });
       element.append( template );
@@ -19,6 +44,8 @@ app.AppView = Backbone.View.extend({
 
   renderProjects: function() {
     var element = $('#projects-list');
+    element.empty();
+    element.append('<h2>Projects</h2>');
     app.projects.each(function(project) {
       var template = _.template($('#project-template').html(),  {name: project.get("name") });
       element.append( template );
@@ -27,6 +54,8 @@ app.AppView = Backbone.View.extend({
 
   renderWorkspaces: function() {
     var element = $('#workspaces-list');
+    element.empty();
+    element.append('<h2>Workspaces</h2>');
     app.workspaces.each(function(workspace) {
       var workspace_template = _.template($('#workspace-template').html(),  { name: workspace.get("name") });
       element.append( workspace_template );
